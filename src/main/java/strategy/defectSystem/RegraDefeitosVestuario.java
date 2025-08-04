35075f3b28d426f2245d6194a11f867991b0bfc8
Caio Freire Silva <115713768+CaioFreireS@users.noreply.github.com>
2025-08-04 20:48:21 -0300

    add: tiposDefeito Repository

--- src/main/java/strategy/defectSystem/RegraDefeitosVestuario.java
+++ src/main/java/strategy/defectSystem/RegraDefeitosVestuario.java
@@ -10,12 +10,7 @@ public class RegraDefeitosVestuario implements IRegraDefeitoStrategy{
     private Map<String,Double> descontoDefeitoVestuario;
 
     public RegraDefeitosVestuario() {
-        descontoDefeitoVestuario = new HashMap<>();
-        descontoDefeitoVestuario.put("rasgo estruturante",0.3);
-        descontoDefeitoVestuario.put("Ausência de botão principal",0.15);
-        descontoDefeitoVestuario.put("ziper parcialmente funcional",0.15);
-        descontoDefeitoVestuario.put("mancha permanente",0.20);
-        descontoDefeitoVestuario.put("desgaste por pilling acentuado",0.10);
+
     }
 
     @Override
